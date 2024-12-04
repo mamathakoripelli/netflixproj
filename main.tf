6649b3d0b07c7fe8d516fa5ad3cfaec5d83cfdbc
mamatha koripelli <Koripellimamatha@gmail.com>
2024-12-04 11:47:22 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+region = "us-east-1"
+}
+
+resource "aws_instance" "one" {
+count = 5
+ami = "ami-0166fe664262f664c"
+instance_type = "t2.medium"
+key_name = "mamathakp"
+vpc_security_group_ids = ["sg-02768082ea8db393e"]
+tags = {
+Name = var.instance_names[count.index]
+}
+}
+
+variable "instance_names" {
+default = ["jenkins", "nexus", "tomcat-1", "tomcat-2", "monitoring"]
+}
