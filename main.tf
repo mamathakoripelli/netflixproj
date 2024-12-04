63e8251881ba07960457f0801dd29d24e1fe8c4c
mamatha koripelli <Koripellimamatha@gmail.com>
2024-12-04 12:56:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ count = 5
 ami = "ami-0166fe664262f664c"
 instance_type = "t2.medium"
 key_name = "mamathakp"
-vpc_security_group_ids = ["sg-02768082ea8db393e"]
+vpc_security_group_ids = ["sg-0e7f44afb3d9a7332"]
 tags = {
 Name = var.instance_names[count.index]
 }
